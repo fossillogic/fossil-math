cb5c9de6141445d683cf935e39203161900f5b92
Michael Gene Brockus (Dreamer) <michaelbrockus@gmail.com>
2025-08-17 21:16:37 -0600

    add guts

---
+++ code/logic/fossil/math/math.h
@@ -0,0 +1,49 @@
+/*
+ * -----------------------------------------------------------------------------
+ * Project: Fossil Logic
+ *
+ * This file is part of the Fossil Logic project, which aims to develop high-
+ * performance, cross-platform applications and libraries. The code contained
+ * herein is subject to the terms and conditions defined in the project license.
+ *
+ * Author: Michael Gene Brockus (Dreamer)
+ *
+ * Copyright (C) 2024 Fossil Logic. All rights reserved.
+ * -----------------------------------------------------------------------------
+ */
+#ifndef FOSSIL_MATH_CORE_H
+#define FOSSIL_MATH_CORE_H
+
+#include <stddef.h>
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+// *****************************************************************************
+// Function prototypes
+// *****************************************************************************
+
+// draft hash algorithm
+
+#ifdef __cplusplus
+}
+#include <stdexcept>
+#include <vector>
+#include <string>
+
+namespace fossil {
+
+namespace math {
+
+
+
+} // namespace math
+
+} // namespace fossil
+
+#endif
+
+#endif /* FOSSIL_MATH_CORE_H */
---
+++ code/tests/cases/test_thread.cpp
@@ -0,0 +1,55 @@
+/*
+ * -----------------------------------------------------------------------------
+ * Project: Fossil Logic
+ *
+ * This file is part of the Fossil Logic project, which aims to develop high-
+ * performance, cross-platform applications and libraries. The code contained
+ * herein is subject to the terms and conditions defined in the project license.
+ *
+ * Author: Michael Gene Brockus (Dreamer)
+ *
+ * Copyright (C) 2024 Fossil Logic. All rights reserved.
+ * -----------------------------------------------------------------------------
+ */
+#include <fossil/pizza/framework.h>
+#include "fossil/math/framework.h"
+
+
+// * * * * * * * * * * * * * * * * * * * * * * * *
+// * Fossil Logic Test Utilities
+// * * * * * * * * * * * * * * * * * * * * * * * *
+// Setup steps for things like test fixtures and
+// mock objects are set here.
+// * * * * * * * * * * * * * * * * * * * * * * * *
+
+FOSSIL_TEST_SUITE(cpp_math_fixture);
+
+FOSSIL_SETUP(cpp_math_fixture) {
+    // Setup the test fixture
+}
+
+FOSSIL_TEARDOWN(cpp_math_fixture) {
+    // Teardown the test fixture
+}
+
+// * * * * * * * * * * * * * * * * * * * * * * * *
+// * Fossil Logic Test Cases
+// * * * * * * * * * * * * * * * * * * * * * * * *
+// The test cases below are provided as samples, inspired
+// by the Meson build system's approach of using test cases
+// as samples for library usage.
+// * * * * * * * * * * * * * * * * * * * * * * * *
+
+FOSSIL_TEST_CASE(cpp_math_test_blank) {
+    ASSUME_ITS_TRUE(1);
+}
+
+// * * * * * * * * * * * * * * * * * * * * * * * *
+// * Fossil Logic Test Pool
+// * * * * * * * * * * * * * * * * * * * * * * * *
+FOSSIL_TEST_GROUP(cpp_math_tests) {
+    // C++ Wrapper Tests
+    FOSSIL_TEST_ADD(cpp_math_fixture, cpp_math_test_blank);
+
+    FOSSIL_TEST_REGISTER(cpp_math_fixture);
+} // end of tests

61430461574415e7d029496dd107f404045f596b
Michael Gene Brockus (Dreamer) <55331536+dreamer-coding@users.noreply.github.com>
2025-08-18 16:36:23 -0700

    Create trig.h

---
+++ code/logic/fossil/math/trig.h
@@ -0,0 +1,85 @@
+/*
+ * -----------------------------------------------------------------------------
+ * Project: Fossil Logic
+ *
+ * This file is part of the Fossil Logic project, which aims to develop high-
+ * performance, cross-platform applications and libraries. The code contained
+ * herein is subject to the terms and conditions defined in the project license.
+ *
+ * Author: Michael Gene Brockus (Dreamer)
+ *
+ * Copyright (C) 2024 Fossil Logic. All rights reserved.
+ * -----------------------------------------------------------------------------
+ */
+#ifndef FOSSIL_MATH_TRIG_H
+#define FOSSIL_MATH_TRIG_H
+
+#include <stddef.h>
+#include <stdint.h>
+
+// ======================================================
+// Constants
+// ======================================================
+#define FOSSIL_MATH_PI 3.14159265358979323846
+#define FOSSIL_MATH_TWO_PI (2.0 * FOSSIL_MATH_PI)
+#define FOSSIL_MATH_HALF_PI (0.5 * FOSSIL_MATH_PI)
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+// *****************************************************************************
+// Function prototypes
+// *****************************************************************************
+
+// ======================================================
+// Conversion
+// ======================================================
+double fossil_math_trig_deg_to_rad(double degrees);
+double fossil_math_trig_rad_to_deg(double radians);
+
+// ======================================================
+// Basic trig functions
+// ======================================================
+double fossil_math_trig_sin(double x);
+double fossil_math_trig_cos(double x);
+double fossil_math_trig_tan(double x);
+
+// Inverse
+double fossil_math_trig_asin(double x);
+double fossil_math_trig_acos(double x);
+double fossil_math_trig_atan(double x);
+double fossil_math_trig_atan2(double y, double x);
+
+// ======================================================
+// Hyperbolic
+// ======================================================
+double fossil_math_trig_sinh(double x);
+double fossil_math_trig_cosh(double x);
+double fossil_math_trig_tanh(double x);
+
+// Inverse hyperbolic
+double fossil_math_trig_asinh(double x);
+double fossil_math_trig_acosh(double x);
+double fossil_math_trig_atanh(double x);
+
+#ifdef __cplusplus
+}
+#include <stdexcept>
+#include <vector>
+#include <string>
+
+namespace fossil {
+
+namespace math {
+
+
+
+} // namespace math
+
+} // namespace fossil
+
+#endif
+
+#endif /* FOSSIL_MATH_TRIG_H */

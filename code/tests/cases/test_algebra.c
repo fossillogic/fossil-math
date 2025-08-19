8045ed18ffca7a7781faf208653f30c5c3e19a44
Michael Gene Brockus (Dreamer) <55331536+dreamer-coding@users.noreply.github.com>
2025-08-19 07:05:30 -0700

    Update test_algebra.c

--- code/tests/cases/test_algebra.c
+++ code/tests/cases/test_algebra.c
@@ -193,7 +193,7 @@ FOSSIL_TEST_GROUP(c_algebra_tests) {
     FOSSIL_TEST_ADD(c_algebra_fixture, c_math_test_solve_quadratic_complex);
     FOSSIL_TEST_ADD(c_algebra_fixture, c_math_test_vector_add);
     FOSSIL_TEST_ADD(c_algebra_fixture, c_math_test_vector_sub);
-    FOSSIL_TEST_ADD(c_algebra_fixture, test_case_c_math_test_dot_product);
+    FOSSIL_TEST_ADD(c_algebra_fixture, c_math_test_dot_product);
 
     FOSSIL_TEST_REGISTER(c_algebra_fixture);
 } // end of tests
